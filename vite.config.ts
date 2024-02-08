8a3a4d93b92680b2df3278f88de2dca5035e12c3
VTTruong287 <truong.vt287@gmail.com>
2024-02-08 07:25:20 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/telekoi-platform/'
+  base: '/telekoi-platform/public/'
 })
