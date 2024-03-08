571b186801f49f151b2bd3f064c4da09249ef5ef
VTTruong287 <truong.vt287@gmail.com>
2024-03-09 03:57:43 +0700

    Update Mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,269 +1,159 @@
-import React from 'react'
-import WebApp from '@twa-dev/sdk'
-import './App.css'
-import { TonConnectButton } from '@tonconnect/ui-react'
-import './App.css'
-import { apiService } from './api/api'
-import { validate } from '@tma.js/init-data-node'
+// import "./App.css";
 
-// import { createHmac } from 'crypto';
+import styles from "./app-styles.module.scss";
 
-type UserInfo = {
-  tgId?: number
-  tgUserName?: string
-  invitedBy?: string
-}
-
-function App() {
-  const [count, setCount] = React.useState(0)
-  const [userInfo, setUserInfo] = React.useState<UserInfo>({})
-  const [allRefCode, setAllRefCode] = React.useState('')
-
-  const [initDataQuery, setInitDataQuery] = React.useState('')
-
-  const [validateErr, setValidateErr] = React.useState<any>(undefined)
-
-  const counterHandler = () => {
-    const randomValue = 1 + Math.floor(Math.random() * 10)
-    setCount((count) => count + randomValue)
-  }
-
-  React.useEffect(() => {
-    const initData = WebApp?.initDataUnsafe;
-    const tgId = initData?.user?.id;
-    const tgUserName = initData?.user?.username;
-    const invitedBy = initData?.start_param;
-
-    if (tgId) {
-      setUserInfo({
-        tgId,
-        tgUserName,
-        invitedBy
-      })
-    }
-  }, [WebApp?.initDataUnsafe])
-
-  const getAllRef = async () => {
-    const rs = await apiService.reference?.getAllReference()
-    console.log('rs: ', rs)
-
-    !!rs && setAllRefCode(JSON.stringify(rs))
-  }
-
-  const saveRefCode = async () => {
-    if (userInfo && userInfo.tgId && userInfo.invitedBy) {
-      const rs = await apiService.reference?.saveRefCode(userInfo.tgId?.toString(), userInfo.invitedBy);
-      if (rs) {
-        WebApp.showPopup({
-          title: 'Save Ref Code',
-          message: 'SUCCESS'
-        })
-      } else {
-        WebApp.showPopup({
-          title: 'Save Ref Code',
-          message: 'FAIL'
-        })
-      }
-    } else {
-      // Prompt error
-      console.log('saveRefCode --- Wrong input data')
-    }
-  }
-
-  const validateInitData = () => {
-    /*
-    Function will throw an error in one of these cases:
-    - auth_date should present integer
-    - auth_date is empty or not found
-    - hash is empty or not found
-    - Signature is invalid
-    - Init data expired
-    */
-   try {
-    const secretToken = "6884381091:AAFoEWqxT2NvaWP4zSu7CsGKpCXt6WhZ8j4"; //import.meta.env.VITE_TELE_SECRET_TOKEN;
-    const initData = WebApp?.initDataUnsafe
-    // const initData = {"query_id":"AAHYMohKAAAAANgyiErugAQg","user":{"id":1250439896,"first_name":"Austin","last_name":"Vu","username":"AusVu1308","language_code":"en","allows_write_to_pm":true},"auth_date":"1709304003","hash":"db081850cd624efaf97624a8d1ca171830e9fb6053035c4031233a6b17a97680"}
-
-    let initDataQueryString = objectToQueryString(initData)
-    
-
-    // console.log('initDataQueryString: ', initDataQueryString)
-
-    setInitDataQuery(initDataQueryString);
-
-    // console.log('initDataQueryString: ', initDataQueryString)
-
-    validate(initDataQueryString, secretToken);
-    // validateInitDataFromTelePlugin(initDataQueryString, secretToken, {expiresIn: 0});
-    console.log('Successfully !!!')
-
-    setValidateErr({message: 'None ERR - PASSED!!!'})
-
-    return true;
-   } catch (error) {
-    // @ts-ignore
-     console.log('ERR - validateInitData: ', error?.message)
-     console.log('ERR - validateInitData 2: ', JSON.stringify(error))
-
-     setValidateErr(error)
-     return false;
-   }
-  }
-
-  // function validateInitDataFromTelePlugin(
-  //   sp: string | URLSearchParams,
-  //   token: string,
-  //   options: ValidateOptions = {},
-  // ): void {
-  //   const searchParams = typeof sp === 'string' ? new URLSearchParams(sp) : sp;
-  
-  //   // Init data creation time.
-  //   let authDate = new Date(0);
-  
-  //   // Init data sign.
-  //   let hash = '';
-  
-  //   // All search params pairs presented as `k=v`.
-  //   const pairs: string[] = [];
-  
-  //   // Iterate over all key-value pairs of parsed parameters and find required
-  //   // parameters.
-  //   searchParams.forEach((value, key) => {
-  //     if (key === 'hash') {
-  //       hash = value;
-  //       return;
-  //     }
-  
-  //     if (key === 'auth_date') {
-  //       const authDateNum = parseInt(value, 10);
-  
-  //       if (Number.isNaN(authDateNum)) {
-  //         throw new TypeError('"auth_date" should present integer');
-  //       }
-  //       authDate = new Date(authDateNum * 1000);
-  //     }
-  
-  //     // Append new pair.
-  //     pairs.push(`${key}=${value}`);
-  //   });
-  
-  //   // Hash and auth date always required.
-  //   if (hash.length === 0) {
-  //     throw new Error('"hash" is empty or not found');
-  //   }
-  
-  //   if (authDate.getTime() === 0) {
-  //     throw new Error('"auth_date" is empty or not found');
-  //   }
-  
-  //   // In case, expiration time passed, we do additional parameters check.
-  //   const { expiresIn = 86400 } = options;
-  
-  //   if (expiresIn > 0) {
-  //     // Check if init data expired.
-  //     if (authDate.getTime() + expiresIn * 1000 < new Date().getTime()) {
-  //       throw new Error('Init data expired');
-  //     }
-  //   }
-  
-  //   // According to docs, we sort all the pairs in alphabetical order.
-  //   pairs.sort();
-  
-  //   // Compute sign.
-  //   const computedHash = createHmac(
-  //     'sha256',
-  //     createHmac('sha256', 'WebAppData').update(token).digest(),
-  //   )
-  //     .update(pairs.join('\n'))
-  //     .digest()
-  //     .toString('hex');
-  
-  //   // In case, our sign is not equal to found one, we should throw an error.
-  //   if (computedHash !== hash) {
-  //     throw new Error('Signature is invalid');
-  //   }
-  // }
-
-  function objectToQueryString(obj: any) {
-    // for (let key of Object.keys(obj)) {
-    //   console.log('key: ', key);
-
-    //   if (!initDataQueryString) {
-    //     initDataQueryString = `${key}=${(obj as any)[key]}`
-    //     continue;
-    //   }
-
-    //   if(key == 'user') {
-    //     initDataQueryString = initDataQueryString.concat(`&${key}=${encodeURIComponent(JSON.stringify((obj as any)[key]))}`)
-    //   } else {
-    //     initDataQueryString = initDataQueryString.concat(`&${key}=${(obj as any)[key]}`)
-    //   }
-    // }
-    
-    const keyValuePairs = [];
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const value = obj[key];
-        if (typeof(value) == 'object') {
-          keyValuePairs.push(`${encodeURIComponent(key)}=${encodeURIComponent(JSON.stringify(value))}`);
-        } else {
-          keyValuePairs.push(`${encodeURIComponent(key)}=${value}`);
-        }
-      }
-    }
-    return keyValuePairs.join('&');
-  }
+const ClaimChestButton = (props: any) => {
+  const { isOpenChest = false, isNextChest = false, children } = props;
 
   return (
+    <div
+      className={[
+        "btn",
+        styles["chest-btn"],
+        isOpenChest ? styles["chest-btn-open"] : styles["chest-btn-normal"],
+        isNextChest ? styles["chest-btn-next"] : "",
+      ].join(" ")}
+    >
+      {children}
+      {isNextChest ? (
+        <img src="/images/svg/next-chest.svg" alt="" height={40} width={40} />
+      ) : (
+        <img src="/images/svg/chest.svg" alt="" height={40} width={40} />
+      )}
+    </div>
+  );
+};
+
+const ClaimChestArea = (props: any) => {
+  return (
+    <div className={styles["claim-chests"]}>
+      <ClaimChestButton isOpenChest={true}>
+        <label>Unlocked</label>
+      </ClaimChestButton>
+      <ClaimChestButton>
+        <div className={styles["chest-title-area"]}>
+          <img src="/images/svg/time.svg" alt="" height={15} width={15} />
+          <label>9:30</label>
+        </div>
+      </ClaimChestButton>
+      <ClaimChestButton>
+        <div className={styles["chest-title-area"]}>
+          <img src="/images/svg/time.svg" alt="" height={15} width={15} />
+          <label>10m</label>
+        </div>
+      </ClaimChestButton>
+      <ClaimChestButton isNextChest={true}>
+        <div className={styles["next-chest-title-area"]}>
+          <label>Next chest</label>
+        </div>
+      </ClaimChestButton>
+    </div>
+  );
+};
+
+function App() {
+  return (
     <>
-      <h1>Telegram mini games v3</h1>
-      <div className="card">
-        <button onClick={getAllRef}>Call Get All Ref API</button>
-        <br />
-        <button onClick={saveRefCode}>Call Add Ref API</button>
-        <br />
-        <button onClick={validateInitData}>Validate Init Data</button>
-        <br />
-        <p>
-          <label>Telegram ID: {userInfo.tgId || '...'}</label>
-        </p>
-        <p>
-          <label>Telegram User Name: {userInfo.tgUserName || '...'}</label>
-        </p>
-        <p>
-          <label>Telegram Invited ID: {userInfo.invitedBy || '...'}</label>
-        </p>
-        <p>
-          <label>Init Data: {JSON.stringify(WebApp?.initDataUnsafe)}</label>
-        </p>
-        <p>
-          <label>Init Data Query: {`${initDataQuery}`}</label>
-        </p>
-        <p>
-          <br />
-          <label>ERR Validation - Init Data: {`${JSON.stringify(validateErr || '...')}`}</label>
-          <br />
-          <label>{` ERROR MSG ${validateErr?.message || '...'}`}</label>
-          <br />
-        </p>
-        <p>
-          <label>All Ref Code: {allRefCode}</label>
-        </p>
+      <div className={["navbar bg-base-100", styles["navbar"]].join(" ")}>
+        <div className="navbar-start">
+          <button className="btn btn-square btn-ghost">
+            <img src={"/images/svg/close.svg"} alt="Close" width={40} height={40} />
+          </button>
+        </div>
+        <div className="navbar-center">
+          <img src={"/images/svg/suzume-logo.svg"} alt="Suzume Logo" width={30} height={30} />
+          <label className="text-xl ml-1">suzume</label>
+        </div>
+        <div className="navbar-end">
+          <img src={"/images/img/coin.png"} alt="Coin" width={20} height={20} />
+          <label className="text-l">123</label>
+        </div>
       </div>
-      <div className="card">
-        <button className='btn btn-primary' onClick={counterHandler}>
-          Click to add random 1-10 
+
+      <div className={styles["body"]}>
+        <div className={[styles["claim-quests"], "card bg-base-100 shadow-xl"].join(" ")}>
+          <div className={styles["quest-center-title"]}>
+            <div className={styles["quest-center-left-area"]}>
+              <h4 className={styles["title"]}>Quest Center</h4>
+              <span className={styles["description"]}>Complete quests and earn rewards Quests refresh every day</span>
+            </div>
+            <div className={styles["quest-center-right-area"]}>
+              <img src={"/images/svg/cards-chest.svg"} alt="Card" width={110} height={110} />
+            </div>
+          </div>
+
+          {/* <div className="card-body p-0"> */}
+            <div role="tablist" className={["tabs tabs-bordered", styles["quest-tabs"]].join(" ")}>
+              {/* DAILY TAB */}
+              <input type="radio" name="quests_tab" role="tab" className="tab" aria-label="Daily" checked />
+              <div role="tabpanel" className={["tab-content", styles["scoll-view-container"]].join(" ")}>
+                <div className={[styles["scroll-view"]].join(" ")}>
+                  <div className={["card", styles["quest-item"]].join(" ")}>
+                    {/* QUEST TITLE */}
+                    <div className={[styles["quest-item-title"]].join(" ")}>
+                      <label>Login Bonus</label>
+                    </div>
+                    {/* QUEST PROGESS */}
+                    <div className={[styles["quest-item-progress"]].join(" ")}>
+                      <progress className="progress progress-info w-100" value="100" max="100"></progress>
+                      <span className={[styles["counter"]].join(" ")}>1/1</span>
+                    </div>
+                    <div className={[styles["quest-item-info-n-actions"]].join(" ")}>
+                      <div className={[styles["info"]].join(" ")}>
+                        {/* COIN INFO */}
+                        <div className={["indicator", styles["info-indicator"]].join(" ")}>
+                          <div className={["indicator-item indicator-bottom", styles["info-indicator-bottom"]].join(" ")}>
+                            <span>x10</span>
+                          </div>
+                          <img src={"/images/img/ticket.png"} alt="Suzume Ticket" width={25} height={25} />
+                        </div>
+                        {/* TICKET INFO */}
+                        <div className={["indicator", styles["info-indicator"]].join(" ")}>
+                        <div className={["indicator-item indicator-bottom", styles["info-indicator-bottom"]].join(" ")}>
+                            <span>x10</span>
+                          </div>
+                          <img src={"/images/img/coin.png"} alt="Suzume Coin" width={25} height={25} />
+                        </div>
+                      </div>
+                      {/* CLAIM BUTTON */}
+                      <div className={[styles["action"]].join(" ")}>
+                        <button className="btn">Claim</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* WEEKLY TAB */}
+              <input type="radio" name="quests_tab" role="tab" className="tab" aria-label="Weekly" />
+              <div role="tabpanel" className="tab-content p-10">Tab content 2</div>
+
+              {/* ACHIVEMENTS TAB */}
+              <input type="radio" name="quests_tab" role="tab" className="tab" aria-label="Achivements" />
+              <div role="tabpanel" className="tab-content p-10">Tab content 3</div>
+
+              {/* FEATURING TAB */}
+              <input type="radio" name="quests_tab" role="tab" className="tab" aria-label="Featuring" />
+              <div role="tabpanel" className="tab-content p-10">Tab content 4</div>
+            </div>
+          </div>
+        {/* </div> */}
+
+        <ClaimChestArea />
+      </div>
+
+      <div className={["btm-nav", styles["bottom-nav"]].join(" ")}>
+        <button className="active">
+        <img src={"/images/svg/home.svg"} alt="Close" width={25} height={25} />
+          <span className="btm-nav-label">HOME</span>
         </button>
-        <br />
-        <p>
-          <label>Total is {count}</label>
-        </p>
-      </div>
-      <div className="card-flex-center">
-        <TonConnectButton />
+        <button>
+        <img src={"/images/svg/wallet.svg"} alt="Close" width={25} height={25} />
+          <span className="btm-nav-label">STATICS</span>
+        </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import { TonConnectUIProvider } from '@tonconnect/ui-react'
+import './index.scss'
 
 const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
-    <App />
-  </TonConnectUIProvider>,
+  <App />
 )
